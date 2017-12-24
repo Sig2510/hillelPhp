c4468c6905d6ceddb5525a3b28725a04f25e2160
semnickolas <semnickolas@gmail.com>
2017-12-24 12:54:32 +0200

    style_changes
    some style changes

--- lecture3/view/post.php
+++ lecture3/view/post.php
@@ -1,31 +1,45 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <title>Hello, world!</title>
+    <title><?php echo $post[0]?></title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <link rel="shortcut icon" href="src\icon\favicon.ico?v=1" type="image/x-icon">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
   </head>
   <body>
+    <nav aria-label="breadcrumb" role="navigation">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item active" aria-current="page"><a href="/">Home</a></li>
+        <div class="container">
+          <div class="text-right">
+            <h7>You are reading '<?php echo $post[0]?>'!</h7>
+          </div>
+        </div>
+      </ol>
+    </nav>
     <div class="container">
       <div class="row justify-content-md-center">
-        <div class="col-md-10">
-            <h2>You are reading '<?php echo $post[0]?>'</h2>
-        </div>
             <div class="col-md-10">
               <div class="jumbotron jumbotron-fluid">
                 <div class="container">
-                    <h1 class="display-3"><?php echo $post[0]; ?></h1>
-                  <p class="lead"><?php echo $post[1]; ?></p>
-                  <p class="lead">author: <?php echo $post[2]; ?></p>
+                  <h1 class="display-3"><?php echo $post[0]; ?></h1>
+                  <div class="font-italic">
+                    <p class="text-justify"><?php echo $post[1]; ?></p>
+                  </div>
+                  <div class="font-weight-bold">
+                    <p class="text-sm-right">author: <?php echo $post[2]; ?></p>
+                  </div>
                 </div>
               </div>
             </div>
-            <div class="col-md-10">
-                <a href="/"><h1>Home page!</h1></a>
+            <div class="container">
+              <div class="text-center">
+                <h1><a class="btn btn-primary" href="/" role="button">Homepage</a></h1>
+              </div>
             </div>
       </div>
     </div>
--- lecture3/view/view.php
+++ lecture3/view/view.php
@@ -1,29 +1,38 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <title>Hello, world!</title>
+    <title>My Blog</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <link rel="shortcut icon" href="src\icon\favicon.ico?v=1" type="image/x-icon">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
   </head>
   <body>
+    <nav aria-label="breadcrumb" role="navigation">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item active" aria-current="page"><a href="/">Home</a></li>
+      </ol>
+    </nav>
     <div class="container">
       <div class="row justify-content-md-center">
         <div class="col-md-center">
-            <a href="/"><h1>Hello, user! This is my first site!</h1></a>
+            <h1>Hello, user! This is my first blog!</h1>
         </div>
           <?php foreach ($posts as $index => $post) { ?>
             <div class="col-md-10">
               <div class="jumbotron jumbotron-fluid">
                 <div class="container">
-                  <a href="/post.php?id=<?php echo $index?>">
+
                     <h1 class="display-3"><?php echo $post[0].' '.($index+1); ?></h1>
+
+                  <p class="lead"><?php echo substr($post[1], 0, 97).'...'; ?></p>
+                  <p class="lead"><i>author: <?php echo $post[2]; ?></i></p>
+                  <a href="/post.php?id=<?php echo $index?>">
+                    <p class="text-right">Read more</p>
                   </a>
-                  <p class="lead"><?php echo $post[1]; ?></p>
-                  <p class="lead">author: <?php echo $post[2]; ?></p>
                 </div>
               </div>
             </div>
