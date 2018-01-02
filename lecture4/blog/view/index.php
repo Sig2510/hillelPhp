73335546482bf822a436c39a98f8a52de39471af
semnickolas <semnickolas@gmail.com>
2018-01-02 21:46:29 +0200

    blog the next)

--- lecture4/blog/view/index.php
+++ lecture4/blog/view/index.php
@@ -1,39 +1,40 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <title>Hello, world!</title>
+    <title>My Blog</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <link rel="shortcut icon" href="src\icon\favicon.ico?v=1" type="image/x-icon">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
   </head>
   <body>
+    <nav aria-label="breadcrumb" role="navigation">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item active" aria-current="page"><a href="/">Home</a></li>
+      </ol>
+    </nav>
     <div class="container">
       <div class="row justify-content-md-center">
-        <div class="col-md-10">
-          <a href="/"><h1>Hello, user!</h1></a>
+        <div class="col-md-center">
+            <h1>Hello, user! This is my first blog!</h1>
         </div>
-        <?php foreach ($posts as $index => $post) { ?>
-          <div class="col-md-10">
-            <div class="jumbotron jumbotron-fluid">
-              <div class="container">
-                <a href="/post.php?id=<?php echo $index; ?>">
-                  <h1 class="display-3">
-                    <?php echo $post['title']; ?>
-                  </h1> 
-                </a>
-                <p class="lead">
-                  <?php echo $post['body']; ?>
-                </p>
-                <p class="lead">
-                  author: <?php echo $post['author']; ?>
-                </p>
+          <?php foreach ($posts as $index => $post) { ?>
+            <div class="col-md-10">
+              <div class="jumbotron jumbotron-fluid">
+                <div class="container">
+                  <h1 class="display-3"><?php echo $post['title']; ?></h1>
+                  <p class="lead"><?php echo substr($post['body'], 0, 97).'...'; ?></p>
+                  <p class="lead"><i>author: <?php echo $post['author']; ?></i></p>
+                  <a href="/post.php?id=<?php echo $index?>">
+                    <p class="text-right">Read more</p>
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
-        <?php } ?>
+          <?php } ?>
       </div>
     </div>
   </body>
--- lecture4/blog/view/post.php
+++ lecture4/blog/view/post.php
@@ -1,36 +1,46 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <title>Hello, world!</title>
+    <title><?php echo $post[0]?></title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <link rel="shortcut icon" href="src\icon\favicon.ico?v=1" type="image/x-icon">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
   </head>
   <body>
+    <nav aria-label="breadcrumb" role="navigation">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item active" aria-current="page"><a href="/">Home</a></li>
+        <div class="container">
+          <div class="text-right">
+            <h7>You are reading '<?php echo $post['title']?>'!</h7>
+          </div>
+        </div>
+      </ol>
+    </nav>
     <div class="container">
       <div class="row justify-content-md-center">
-        <div class="col-md-10">
-          <a href="/"><h1>Hello, user!</h1></a>
-          <h2>You are reading '<?php echo $post['title']; ?>'</h2>
-        </div>
-          <div class="col-md-10">
-            <div class="jumbotron jumbotron-fluid">
-              <div class="container">
-                <h1 class="display-3">
-                  <?php echo $post['title']; ?>
-                </h1>
-                <p class="lead">
-                  <?php echo $post['body']; ?>
-                </p>
-                <p class="lead">
-                  author: <?php echo $post['author']; ?>
-                </p>
+            <div class="col-md-10">
+              <div class="jumbotron jumbotron-fluid">
+                <div class="container">
+                  <h1 class="display-3"><?php echo $post['title']; ?></h1>
+                  <div class="font-italic">
+                    <p class="text-justify"; style="text-indent: 25px"><?php echo $post['body']; ?></p>
+                  </div>
+                  <div class="font-weight-bold">
+                    <p class="text-sm-right">author: <?php echo $post['author']; ?></p>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
+            <div class="container">
+              <div class="text-center">
+                <h1><a class="btn btn-primary" href="/" role="button">Homepage</a></h1>
+              </div>
+            </div>
       </div>
     </div>
   </body>
